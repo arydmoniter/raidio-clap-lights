c777016cbd37af75c55ec5e62448ef8df5b24bf9
arydmoniter <161125037+arydmoniter@users.noreply.github.com>
2024-05-20 19:48:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
+input.onSound(DetectedSound.Loud, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(1)
